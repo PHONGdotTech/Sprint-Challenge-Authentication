9f73403b0af148fdf53fbf14e654a2ff36df41ce
Phong Yang <phongyang88@hotmail.com>
2020-02-28 10:49:47 -0600

    removed word secret on argument inside verify function inside the authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const {authorization} = req.headers;
 
   if (authorization){
-    jwt.verify(authorization, secrets.JWT_SECRET, (err, decodedToken) => {
+    jwt.verify(authorization, JWT_SECRET, (err, decodedToken) => {
       if (err) {
           res.status(401).json({ message: 'Invalid Credentials' });
         } else {
