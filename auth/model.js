11f2337e19b43eecf67cc6e17561b2c790a5c627
Phong Yang <phongyang88@hotmail.com>
2020-02-28 10:15:24 -0600

    added model for auth

---
+++ auth/model.js
@@ -0,0 +1,14 @@
+const db = require('../database/dbConfig')
+
+module.exports = {
+    add,
+    findBy
+}
+
+function add(user){
+    return db('users').insert(user, "id")
+}
+
+function findBy(filter){
+    return db('users').where(filter).first()
+}
