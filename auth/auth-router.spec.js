9cb15c1f92682e551c2ec8edfc5eed033f945a91
Phong Yang <phongyang88@hotmail.com>
2020-02-28 12:20:05 -0600

    added tests for login endpoint. for each test, a new user is created and then is logged in. tests for status 200, and welcome message and token

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -5,13 +5,10 @@ const db = require('../database/dbConfig')
 
 describe('auth router', () => {
     describe('POST /api/auth/register', () => {
-        
-
         it('should return 201 created', async () => {
-            return request(server).post('/api/auth/register')
+            await request(server).post('/api/auth/register')
                 .send({username: randomizeName(), password: "password"})
                 .then(async res =>{
-                    await db('users').where({id: res.body.id}).delete();
                     expect(res.status).toBe(201)
                 })
         })
@@ -20,13 +17,47 @@ describe('auth router', () => {
             await request(server).post('/api/auth/register')
                 .send({username: randomizeName(), password: "password"})
                 .then(async res =>{
-                    await db('users').where({id: res.body.id}).delete();
                     expect(res.body).toEqual(expect.objectContaining({
                         message: "Successfully created user.",
                         id: expect.any(Number),
                         username: expect.any(String)
                     }))
                 })
+        })
+    })
+
+    describe('POST to /api/auth/register then POST to /api/auth/login', () => {
+        it('should return status 200 ok', async () => {
+            const name = await randomizeName()
+            await request(server).post('/api/auth/register')
+                .send({username: name, password: "password"})
+                .then(async res =>{
+                    await request(server).post('/api/auth/login')
+                    .send({username: name, password: "password"})
+                    .then(async res2 =>{
+                        expect(res2.status).toBe(200)
+                    })
+                })
+        })
+
+        it('should return welcome-back message and a token', async () => {
+            const name = await randomizeName()
+            await request(server).post('/api/auth/register')
+                .send({username: name, password: "password"})
+                .then(async res =>{
+                    await request(server).post('/api/auth/login')
+                    .send({username: name, password: "password"})
+                    .then(async res2 =>{
+                        expect(res2.body).toEqual(expect.objectContaining({
+                            message: `Welcome back, ${name}!`,
+                            token: expect.any(String)
+                        }))
+                    })
+                })
+        })
+
+        it('truncates db after testing', async () => {
+            await db('users').truncate();
         })
     })
 })
